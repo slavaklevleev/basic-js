10bed9b59932663e165dcaf1cbacb0cf061093ff
slavaklevleev <slava.klevleev@gmail.com>
2021-10-02 19:05:14 +0300

    feat: vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,102 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    str = str.toUpperCase().split("");
+
+    let koef = Math.ceil(str.length / key.length);
+    key = key.toUpperCase().repeat(koef).split("");
+
+    for (let i = 0; i < str.length; i++) {
+      if (!str[i].match(/[A-Z]/g)) {
+        key.splice(i, 0, str[i]);
+      }
+    }
+
+    key = key.join("");
+
+    let codeA = "A".charCodeAt();
+    let lettersCount = "Z".charCodeAt() - "A".charCodeAt() + 1;
+
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+      if (!str[i].match(/[A-Z]/g)) {
+        result += str[i];
+      } else {
+        let letterCode = str[i].charCodeAt() - codeA;
+        let shift = key[i].charCodeAt() - codeA;
+        let letter = String.fromCharCode(
+          codeA + ((letterCode + shift) % lettersCount)
+        );
+        result += letter;
+      }
+    }
+
+    return this.mode === false ? result.split("").reverse().join("") : result;
+  }
+
+  decrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    str = str.toUpperCase().split("");
+
+    let koef = Math.ceil(str.length / key.length);
+    key = key.toUpperCase().repeat(koef).split("");
+
+    for (let i = 0; i < str.length; i++) {
+      if (!str[i].match(/[A-Z]/g)) {
+        key.splice(i, 0, str[i]);
+      }
+    }
+
+    key = key.join("");
+
+    let codeA = "A".charCodeAt();
+    let abcCount = "Z".charCodeAt() - "A".charCodeAt() + 1;
+
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+      if (!str[i].match(/[A-Z]/g)) {
+        result += str[i];
+      } else {
+        let letterCode = str[i].charCodeAt() - codeA;
+        let shift = key[i].charCodeAt() - codeA;
+        let letter = String.fromCharCode(
+          codeA + ((letterCode - shift + abcCount) % abcCount)
+        );
+        result += letter;
+      }
+    }
+
+    return this.mode === false ? result.split("").reverse().join("") : result;
   }
 }
