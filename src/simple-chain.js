769646328c4873a1a238c16583bbf079bdd64584
slavaklevleev <slava.klevleev@gmail.com>
2021-10-02 14:42:22 +0300

    feat: simple chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,34 @@
-import { NotImplementedError } from '../extensions/index.js';
-
-/**
- * Implement chainMaker object according to task description
- * 
- */
 export default {
+  object: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.object.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value === "") {
+      this.object.push("( )");
+    } else {
+      this.object.push(`( ${value} )`);
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (position > this.object.length || position <= 0 || isNaN(position)) {
+      this.object = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.object = this.object
+      .slice(0, position - 1)
+      .concat(this.object.slice(position));
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.object.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let result = this.object.join("~~");
+    this.object = [];
+    return result;
+  },
 };
